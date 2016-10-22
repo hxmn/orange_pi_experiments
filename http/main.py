00294d4db82c83f9b3cafe8301509ff962ee38dc
Ilya <ashikhmin.ilya@gmail.com>
2016-10-22 14:00:16 +0300

    remove heavy version

--- http/main.py
+++ http/main.py
@@ -1,97 +1,53 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import os
-import tornado.gen
-import tornado.httpserver
-import tornado.ioloop
-import tornado.web
-import tornado.wsgi
-import uuid
-from time import time
-from tornado.log import app_log as log
-from tornado.options import define, options
 
-from wsrpc import WebSocketRoute, WebSocket, wsrpc_static
+import json
+from tornado import websocket, web, ioloop
 
-define("listen", default='0.0.0.0', help="run on the given host")
-define("port", default=9090, help="run on the given port", type=int)
-define("pool_size", default=50, help="Default threadpool size", type=int)
-define("debug", default=False, help="Debug", type=bool)
-define("gzip", default=True, help="GZIP responses", type=bool)
-
-COOKIE_SECRET = str(uuid.uuid4())
+cl = []
 
 
-class Application(tornado.web.Application):
-    def __init__(self):
-        project_root = os.path.dirname(os.path.abspath(__file__))
-        handlers = (
-            wsrpc_static(r'/js/(.*)'),
-            (r"/ws/", WebSocket),
-            (r'/(.*)', tornado.web.StaticFileHandler, {
-                'path': os.path.join(project_root, 'static'),
-                'default_filename': 'index.html'
-            }),
-        )
-
-        tornado.web.Application.__init__(
-            self,
-            handlers,
-            xsrf_cookies=False,
-            cookie_secret=COOKIE_SECRET,
-            debug=options.debug,
-            reload=options.debug,
-            gzip=options.gzip,
-        )
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
 
 
-class TestRoute(WebSocketRoute):
-    def init(self, **kwargs):
-        return kwargs
-
-    @tornado.gen.coroutine
-    def delayed(self, delay=0):
-        future = tornado.gen.Future()
-        tornado.ioloop.IOLoop.instance().call_later(delay, lambda: future.set_result(True))
-        yield future
-        raise tornado.gen.Return("I'm delayed {0} seconds".format(delay))
-
-    def getEpoch(self):
-        return time()
-
-    def requiredArgument(self, myarg):
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
         return True
 
-    def _secure_method(self):
-        return 'WTF???'
-
-    @tornado.gen.coroutine
-    def time(self, *args, **kwargs):
-        for i in range(0, 10000):
-            yield self.socket.call('print', a='hello %04d' % i)
-
-class EchoWebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
-        print("WebSocket opened")
-
-    def on_message(self, message):
-        self.write_message(u"You said: " + message)
+        if self not in cl:
+            cl.append(self)
 
     def on_close(self):
-        print("WebSocket closed")
-
-WebSocket.ROUTES['test'] = TestRoute
-
-
-def main():
-    tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(options.port, address=options.listen)
-    log.info('Server started {host}:{port}'.format(host=options.listen, port=options.port))
-    tornado.ioloop.IOLoop.instance().start()
+        if self in cl:
+            cl.remove(self)
 
 
-if __name__ == "__main__":
-    exit(main())
-else:
-    application = tornado.wsgi.WSGIAdapter(Application())
+class ApiHandler(web.RequestHandler):
+    @web.asynchronous
+    def get(self, *args):
+        self.finish()
+        id = self.get_argument("id")
+        value = self.get_argument("value")
+        data = {"id": id, "value": value}
+        data = json.dumps(data)
+        for c in cl:
+            for k in range(0, 1000):
+                c.write_message(str(k))
+
+    @web.asynchronous
+    def post(self):
+        pass
+
+
+app = web.Application([
+    (r'/', IndexHandler),
+    (r'/ws', SocketHandler),
+    (r'/api', ApiHandler),
+])
+
+if __name__ == '__main__':
+    app.listen(8888)
+    ioloop.IOLoop.instance().start()
